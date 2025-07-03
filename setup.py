623abd6933064f12cee02ae382f6d149b5661aa6
MD MAFJUJUL KARIM <107805017+Mahfujul-01726@users.noreply.github.com>
2025-07-03 17:13:04 +0600

    Notebook Experiment Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='source_code_analysis_app',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+        # Add required packages here
+    ],
+)
